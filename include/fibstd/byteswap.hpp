7e89d60f55899f58c26fd82000880895b5385035
Lukas Neverauskis <l.neverauskis@nanoavionics.com>
2022-01-10 15:22:17 +0200

    upd: strip down byteswap impl

--- include/fibstd/byteswap.hpp
+++ include/fibstd/byteswap.hpp
@@ -1,6 +1,4 @@
 /**
- * @ref https://github.com/bruxisma/byteswap
- *
  * Copyright 2018 Isabella Muerte
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -20,6 +18,10 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
+ *
+ * @ref https://github.com/bruxisma/byteswap
+ *
+ * @note Stripped down by Lukas Neverauskis.
  */
 
 #pragma once
@@ -30,88 +32,34 @@
 namespace Fib::Std
 {
 
-namespace _byteswap
-{
-#define IXM_IS_CONSTEXPR(...) noexcept(is_constexpr(__VA_ARGS__))
-
-template <class T> constexpr void is_constexpr(T &&t)
-{
-}
-
 template <class T> constexpr auto as_unsigned(T value) noexcept
 {
     return static_cast<std::make_unsigned_t<T>>(value);
 }
 
-#if defined(_MSC_VER)
-inline auto bswap(std::uint64_t v) noexcept
-{
-    return _byteswap_uint64(v);
-}
-inline auto bswap(std::uint32_t v) noexcept
-{
-    return _byteswap_ulong(v);
-}
-inline auto bswap(std::uint16_t v) noexcept
-{
-    return _byteswap_ushort(v);
-}
-
-constexpr auto const_bswap(std::uint64_t v) noexcept
-{
-    return ((v & UINT64_C(0x0000'0000'0000'00FF)) << 56) | //
-           ((v & UINT64_C(0x0000'0000'0000'FF00)) << 40) | //
-           ((v & UINT64_C(0x0000'0000'00FF'0000)) << 24) | //
-           ((v & UINT64_C(0x0000'0000'FF00'0000)) << 8) |  //
-           ((v & UINT64_C(0x0000'00FF'0000'0000)) >> 8) |  //
-           ((v & UINT64_C(0x0000'FF00'0000'0000)) >> 24) | //
-           ((v & UINT64_C(0x00FF'0000'0000'0000)) >> 40) | //
-           ((v & UINT64_C(0xFF00'0000'0000'0000)) >> 56);
-}
-
-constexpr auto const_bswap(std::uint32_t v) noexcept
-{
-    return ((v & UINT32_C(0x0000'00FF)) << 24) | //
-           ((v & UINT32_C(0x0000'FF00)) << 8) |  //
-           ((v & UINT32_C(0x00FF'0000)) >> 8) |  //
-           ((v & UINT32_C(0xFF00'0000)) >> 24);
-}
-
-constexpr auto const_bswap(std::uint16_t v) noexcept
-{
-    return ((v & UINT16_C(0x00FF)) << 8) | ((v & UINT16_C(0xFF00)) >> 8);
-}
-
-#else
-constexpr auto bswap(std::uint64_t v) noexcept
+constexpr auto _byteswap(std::uint64_t v) noexcept
 {
     return __builtin_bswap64(v);
 }
-constexpr auto bswap(std::uint32_t v) noexcept
+constexpr auto _byteswap(std::uint32_t v) noexcept
 {
     return __builtin_bswap32(v);
 }
-constexpr auto bswap(std::uint16_t v) noexcept
+constexpr auto _byteswap(std::uint16_t v) noexcept
 {
     return __builtin_bswap16(v);
 }
-#endif /* defined(_MSC_VER) */
-} // namespace _byteswap
 
-template <class IntegerType,
-          class = std::enable_if_t<std::is_integral_v<IntegerType>>>
+template <class IntegerType, class = std::enable_if_t<std::is_integral_v<IntegerType>>>
 constexpr IntegerType byteswap(IntegerType value) noexcept
 {
     if constexpr (sizeof(IntegerType) == 1)
     {
         return value;
     }
-#if defined(_MSC_VER)
-    if constexpr (IXM_IS_CONSTEXPR(value))
+    else
     {
-        return _byteswap::const_bswap(_byteswap::as_unsigned(value));
+        return _byteswap(as_unsigned(value));
     }
-#endif /* defined(_MSC_VER) */
-    return _byteswap::bswap(_byteswap::as_unsigned(value));
 }
 } // namespace Fib::Std
--- tests/byteswapTests.cpp
+++ tests/byteswapTests.cpp
@@ -10,9 +10,14 @@ TEST_CASE("endian")
         REQUIRE(input == Fib::Std::byteswap(Fib::Std::byteswap(input)));
     }
 
-    SECTION("byteswap64")
+    SECTION("byteswap8")
     {
-        const uint64_t input = 0x8877665544332211, expectedOutput = 0x1122334455667788;
+        const uint8_t input = 0x12, expectedOutput = 0x12;
+        REQUIRE(expectedOutput == Fib::Std::byteswap(input));
+    }
+    SECTION("byteswap16")
+    {
+        const uint16_t input = 0x2211, expectedOutput = 0x1122;
         REQUIRE(expectedOutput == Fib::Std::byteswap(input));
     }
     SECTION("byteswap32")
@@ -20,9 +25,9 @@ TEST_CASE("endian")
         const uint32_t input = 0x44332211, expectedOutput = 0x11223344;
         REQUIRE(expectedOutput == Fib::Std::byteswap(input));
     }
-    SECTION("byteswap16")
+    SECTION("byteswap64")
     {
-        const uint16_t input = 0x2211, expectedOutput = 0x1122;
+        const uint64_t input = 0x8877665544332211, expectedOutput = 0x1122334455667788;
         REQUIRE(expectedOutput == Fib::Std::byteswap(input));
     }
 
@@ -37,9 +42,14 @@ TEST_CASE("endian")
         REQUIRE(expected == Fib::Std::byteswap(u16In32Frame));
     }
 
-    SECTION("byteswap64CompileTime")
+    SECTION("byteswap8CompileTime")
     {
-        constexpr uint64_t input = 0x8877665544332211, expectedOutput = 0x1122334455667788;
+        constexpr uint8_t input = 0x12, expectedOutput = 0x12;
+        static_assert(expectedOutput == Fib::Std::byteswap(input));
+    }
+    SECTION("byteswap16CompileTime")
+    {
+        constexpr uint16_t input = 0x2211, expectedOutput = 0x1122;
         static_assert(expectedOutput == Fib::Std::byteswap(input));
     }
     SECTION("byteswap32CompileTime")
@@ -47,9 +57,9 @@ TEST_CASE("endian")
         constexpr uint32_t input = 0x44332211, expectedOutput = 0x11223344;
         static_assert(expectedOutput == Fib::Std::byteswap(input));
     }
-    SECTION("byteswap16CompileTime")
+    SECTION("byteswap64CompileTime")
     {
-        constexpr uint16_t input = 0x2211, expectedOutput = 0x1122;
+        constexpr uint64_t input = 0x8877665544332211, expectedOutput = 0x1122334455667788;
         static_assert(expectedOutput == Fib::Std::byteswap(input));
     }
 }
